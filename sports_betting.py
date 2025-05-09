5990b6374ce9d09a5b1d02f6022dc9123b31a93b
andkntr <75658421+andkntr@users.noreply.github.com>
2025-05-09 12:46:13 +0900

    Update sports_betting.py

--- sports_betting.py
+++ sports_betting.py
@@ -39,7 +39,7 @@ def auth_page():
         "TOTTEIアプリのトップ画面に表示されている「TOTTEI ID」を入力してください。（例：1000001234 の場合 → 1234）", 
         key="auth_id"
     )
-    nickname = st.text_input("ニックネーム（アリーナなどで表示される場合があります）", key="nickname")
+    nickname = st.text_input("ニックネーム（結果発表時にお呼びします）", key="nickname")
     password = st.text_input("合言葉（チラシをご確認ください。）", type="password")
 
     if st.button("認証して予想へ進む"):
